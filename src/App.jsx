43ab5772e8db0761c35fd4540995b8ca2cc7e22a
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-03 20:04:08 +0800

    add a homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Homepage from "./pages/homepage"
+
 function App() {
   return (
     <>
+      <Homepage />
     </>
   )
 }
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,13 @@
+import Herobanner from "../components/herobanner";
+import Navbar from "../components/navbar";
+
+function Homepage() {
+    return (
+        <>
+            <Navbar />
+            <Herobanner />
+        </>
+    );
+}
+
+export default Homepage
